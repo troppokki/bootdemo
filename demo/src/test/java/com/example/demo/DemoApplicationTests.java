bd3ab45aab474db555b9d3848cf2b01559355243
troppokki <troppokki@DESKTOP-GHSMO24>
2023-09-10 21:42:44 +0900

    테스트 되니?

--- demo/src/test/java/com/example/demo/DemoApplicationTests.java
+++ demo/src/test/java/com/example/demo/DemoApplicationTests.java
@@ -1,26 +1,14 @@
 package com.example.demo;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
 
-@RestController
-@SpringBootApplication
-public class DemoApplication {
+@SpringBootTest
+class DemoApplicationTests {
 
-	
-	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
+	@Test
+	void contextLoads() {
+		//여기 코드를 입력합니다.
 	}
-	
-	//localhost:8080를 호출하면 helloWorld를 찍어주기 위한 함수
-	@RequestMapping("/")
-	public String helloWorld(){
-		return "helloWorld";
-	}
-	
-	
-	//수정합니다.  
-
+//정상 commit push가 됩니까ㅣ?
 }
