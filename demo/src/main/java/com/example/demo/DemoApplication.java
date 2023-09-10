b717533a6eb691bb5102a397b13a7b7cd5d64476
troppokki <troppokki@DESKTOP-GHSMO24>
2023-09-10 20:53:46 +0900

    최초 레파지토리 등록

---
+++ demo/src/main/java/com/example/demo/DemoApplication.java
@@ -0,0 +1,23 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@SpringBootApplication
+public class DemoApplication {
+
+	
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+	
+	//localhost:8080를 호출하면 helloWorld를 찍어주기 위한 함수
+	@RequestMapping("/")
+	public String helloWorld(){
+		return "helloWorld";
+	}
+
+}
